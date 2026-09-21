225d9f0c5210a26f98e05a84a42a1ccb1f527411
Luong Ngoc Anh <trannhatanh3112@gmail.com>
2026-09-22 00:01:03 +0800

    Add label pipeline (reuse from lab2 & modify)

--- main.py
+++ main.py
@@ -13,11 +13,32 @@ import pyspark.sql.functions as F
 from pyspark.sql.functions import col
 from pyspark.sql.types import StringType, IntegerType, FloatType, DateType
 
+from utils.label_pipeline import build_label_pipeline
 
 
 
 def main():
-    print("MLE Assignment 1 pipeline started")
+    # Initialize SparkSession
+    spark = pyspark.sql.SparkSession.builder \
+        .appName("MLE Assignment 1") \
+        .master("local[*]") \
+        .getOrCreate()
+
+    # Set log level to ERROR to hide warnings
+    spark.sparkContext.setLogLevel("ERROR")
+
+    # set up config
+    snapshot_date_str = "2023-01-01"
+    start_date_str = "2023-01-01"
+    end_date_str = "2024-12-01"    
+
+    # start label pipeline
+    print("Starting label pipeline...")
+    build_label_pipeline(spark, start_date_str, end_date_str)
+    print("Label pipeline completed.")
+
+
+    # Start feature pipeline
 
 
 if __name__ == "__main__":
